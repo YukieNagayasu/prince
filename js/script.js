fde9ae569e926fd48071bf0aae68bfeb800dabd4
Yukie Nagayasu <mtmtanz@yahoo.co.jp>
2020-11-30 23:23:15 +0900

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+$('#slider').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear'
+  
+});
+
+$('#slider_sp').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear'
+  
+});
+
 $('.burger-btn').on('click',function(){
   $('.burger-btn').toggleClass('close');
   $('.hamburger-wrapper').fadeToggle(500);
