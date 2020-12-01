c413e268ad204dc0c149505b504bf8ebe46d3844
Yukie Nagayasu <mtmtanz@yahoo.co.jp>
2020-12-01 23:52:23 +0900

    グローバルナビドロップダウン

--- js/script.js
+++ js/script.js
@@ -14,11 +14,16 @@ $('#slider_sp').slick({
   arrows: false,
   infinite: true,
   fade: true,
-  cssEase: 'linear'
-  
+  cssEase: 'linear'  
 });
 
 $('.burger-btn').on('click',function(){
   $('.burger-btn').toggleClass('close');
   $('.hamburger-wrapper').fadeToggle(500);
+});
+
+$('.nav-item').hover(function(){
+  $("ul:not(:animated)", this).slideDown();
+}, function(){
+  $("ul.dropdwn_menu", this).slideUp();
 });
