d877dc7a2ad57cde93b0de6dc422e79d4825ee70
Yukie Nagayasu <mtmtanz@yahoo.co.jp>
2020-12-02 23:59:47 +0900

    スクロールイベント

--- js/script.js
+++ js/script.js
@@ -27,3 +27,16 @@ $('.nav-item').hover(function(){
 }, function(){
   $("ul.dropdwn_menu", this).slideUp();
 });
+
+$(function(){
+  $(window).scroll(function (){
+    $('.effect-fade').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 100){
+        $(this).addClass('effect-scroll');
+      }
+    });
+  });
+});
